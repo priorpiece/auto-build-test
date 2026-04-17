20fe7894a6dd06bfa2ee7145dffcc78abec8bedf
JeHee <ljh945999@gmail.com>
2026-04-17 14:22:55 +0900

    feat : add test Controller

---
+++ src/main/java/com/test/build/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.test.build.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String getHello(){
+        return "hello";
+    }
+}
